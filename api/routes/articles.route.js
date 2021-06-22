b91c9a1425114fd89e6c27adea76630f2cd17f25
Javier Raez Labrado <jrlabra2@gmail.com>
2021-06-22 23:43:49 +0200

    auth for every editing method

--- api/routes/articles.route.js
+++ api/routes/articles.route.js
@@ -1,17 +1,18 @@
 import express from "express";
 import ArticlesCtrl from "../controllers/articles.controller.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 router
   .route("/")
   .get(ArticlesCtrl.apiGetArticles)
-  .post(ArticlesCtrl.apiPostArticle);
+  .post(auth, ArticlesCtrl.apiPostArticle);
 
 router
   .route("/:articleId")
   .get(ArticlesCtrl.apigetArticleById)
-  .put(ArticlesCtrl.apiUpdateArticle)
-  .delete(ArticlesCtrl.apiDeleteArticle);
+  .put(auth, ArticlesCtrl.apiUpdateArticle)
+  .delete(auth, ArticlesCtrl.apiDeleteArticle);
 
 export default router;
--- api/routes/categories.route.js
+++ api/routes/categories.route.js
@@ -1,20 +1,21 @@
 import express from "express";
 import CategoriesCtrl from "../controllers/categories.controller.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 router
   .route("/")
   .get(CategoriesCtrl.apiGetCategories)
-  .post(CategoriesCtrl.apiPostCategory);
+  .post(auth, CategoriesCtrl.apiPostCategory);
 
 router.route("/maincategories").get(CategoriesCtrl.apiGetMainCategories);
 
 router
   .route("/:categoryId")
   .get(CategoriesCtrl.apiGetCategory)
-  .put(CategoriesCtrl.apiUpdateCategory)
-  .delete(CategoriesCtrl.apiDeleteCategory);
+  .put(auth, CategoriesCtrl.apiUpdateCategory)
+  .delete(auth, CategoriesCtrl.apiDeleteCategory);
 
 router
   .route("/:mainId/subcategories")
